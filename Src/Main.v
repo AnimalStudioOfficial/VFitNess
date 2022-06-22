d6253a60df9d3331196d25ee49e917342ee20fb8
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-06-22 19:16:27 -0400

    Update Main.v

--- Src/Main.v
+++ Src/Main.v
@@ -1,12 +1,38 @@
 import os
+import term
 
 const (
 	debug_mode = true
 	version = "0.9"
 )
 
+fn quit_promt(){
+	term.clear() // clears the content in the terminal
+    width, height := term.get_terminal_size() // get the size of the terminal
+    term.set_cursor_position(x: width / 2, y: height / 2) // now we point the cursor to the middle of  the terminal
+    //println(term.strikethrough(term.bright_green('hello world'))) // Print green text
+    term.set_cursor_position(x: 0, y: height) // Sets the position of the cursor to the bottom of the terminal
+    // Keep prompting until the user presses the q key
+    for {
+        if var := os.input_opt('press q to quit: ') {
+            if var != 'q' {
+                continue
+            }
+            break
+        }
+        println('')
+        break
+    }
+    println('Goodbye.')
+	exit(0)
+}
+
 fn about_cmd(){
+term.clear() // clears the content in the terminal
 println("VFitNess "+version)
+println("------------------")
+println("By AnimalStudioOfficial")
+println("Made with Vlang")
 }
 
 fn main(){
@@ -15,7 +41,7 @@ fn main(){
  user_input := os.input('What do you want to do:')
 match user_input {
 	'about' { about_cmd() }
-	//'linux' { println('Linux.') }
+	'quit' { exit(0) }
 	else { println(user_input+"is NOT a command") }
 }
 
