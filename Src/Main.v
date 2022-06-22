223d4001e3debe58fae88012aac82ef1b7deaee6
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-06-22 19:12:13 -0400

    Added About command

--- Src/Main.v
+++ Src/Main.v
@@ -2,14 +2,19 @@ import os
 
 const (
 	debug_mode = true
+	version = "0.9"
 )
+
+fn about_cmd(){
+println("VFitNess "+version)
+}
 
 fn main(){
  println("Welcome To VFitNess")
  // read text from stdin
  user_input := os.input('What do you want to do:')
 match user_input {
-	'about' { println('') }
+	'about' { about_cmd() }
 	//'linux' { println('Linux.') }
 	else { println(user_input+"is NOT a command") }
 }
