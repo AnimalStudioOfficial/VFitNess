822aeeab8913c27d80728d05ebeee504a96510e7
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-07-01 14:16:07 -0400

    Update Sorter.v

--- tests/Sorter/Sorter.v
+++ tests/Sorter/Sorter.v
@@ -2,4 +2,21 @@ import os
 
 fn main(){
 println("Welcome to Sorter")
+
+ files := os.ls('tosort')?
+ mut count := 0
+ if files.len > 0 {
+     for file in files {
+         if file.ends_with('.v') {
+             os.mv(file, 'build/') or {
+                println('err: $err')
+                
+              }
+         }
+         count++
+     }
+ }
+ if count == 0 {
+     println('No files')
+ }
 }
